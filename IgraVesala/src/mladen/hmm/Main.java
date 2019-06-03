e6c12607f55c83bc54da0cfbee3600e42e4fb383
zenath85 <49948536+zenath85@users.noreply.github.com>
2019-06-03 18:48:10 +0200

    Add files via upload

--- IgraVesala/src/mladen/hmm/Main.java
+++ IgraVesala/src/mladen/hmm/Main.java
@@ -1,19 +1,40 @@
 package mladen.hmm;
 
+import java.io.File;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Exception {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	// write your code here
 
+
+
+        // Ubacivanje  Reci u Array listu tj spisak svih dostupnih reci
+
+        File file = new File("C:\\Users\\A392662\\Desktop\\JavaProjekti\\IgraVesala\\src\\mladen\\hmm\\BazaReci");
+        Scanner scanner1 = new Scanner(file);
+        ArrayList<String> listaReci = new ArrayList<>();
+        int lineNumber = 1;
+        while (scanner1.hasNext()){
+            listaReci.add(scanner1.nextLine());
+            lineNumber++;
+        }
+        scanner1.close();
+
+// Incijalizacija rece - UZIMA SE RANDOM REC IZ BazeReci
+
+        Random random = new Random();
+        int izvucanaRec =(random.nextInt(lineNumber)) ;
+        char [] charArray =listaReci.get(izvucanaRec).toCharArray();
+
+
+
         Scanner scanner = new Scanner(System.in);
-
-
         int pokusaj =6;
-// Incijalizacija rece - idealno treba zameniti sa random reci iz fajla
-        char  [] charArray =  {'m', 'l','a', 'd','e','b'};
         int duzina = charArray.length;
 
 //SKRIVENA REC
@@ -31,6 +52,8 @@ public class Main {
 
         System.out.println();
 
+        int pogodjenaSlova =0;
+
 
         while(pokusaj>0){
             System.out.println();
@@ -39,36 +62,37 @@ public class Main {
             char unos = scanner.next("[a-zA-Z]").toLowerCase().charAt(0);
 
 
-            int pogodjenaSlova =0;
-
             for (int i =0;i<charArray.length;i++)
                 if(charArray[i]==unos) {
                     pogodjenaSlova++;
                     skrivenArray[i]=unos;
                 }
 
+            System.out.println("Broj Pogodjenih SLOVA "+pogodjenaSlova);
+
             if(pogodjenaSlova==0){
                 pokusaj--;
                 System.out.println("TRAZENO SLOVO NE POSTOJI");
                 System.out.println();
                 System.out.println("Broj pokusaja je "+pokusaj);
+                pogodjenaSlova=0;
             }
             else
-                System.out.println("Broj pokusaja je "+pokusaj);
-
+            System.out.println("Broj pokusaja je "+pokusaj);
 
             stampajrec(skrivenArray);
 
-            if(isWordCompleted(skrivenArray)) {
+
+            if(pogodjenaSlova==duzina) {
                 System.out.println("USPESNO STE RESILI SKRIVENU REC");
                 System.exit(0);
 
 
             }
-
+            pogodjenaSlova=0;
         }
 
-    }
+    } // END OF MAIN
 
 
  // Methoda koja stampa skrivenu rec
@@ -82,18 +106,8 @@ public class Main {
 
 // provera da li je trazena rec kompletirana
 
-    public static boolean isWordCompleted(char passArra[]){
-        for(int i =0; i<passArra.length;i++) {
-
-            if (passArra[i]=='_')
-            return false;
-        }
-        return true;
 
 
 
-    } // kraj methode
 
-
-
-}
+}// END OF CLASS
